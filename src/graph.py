f583575aa583b8ac5710fc2dae01d5e6742fef5b
violeta <159485730+violeta456@users.noreply.github.com>
2026-07-25 09:06:35 -0500

    .

--- src/graph.py
+++ src/graph.py
@@ -1,16 +1,1 @@
-from langgraph.graph import END, StateGraph
-from langgraph.prebuilt import ToolNode, tools_condition
 
-from src.agents.chat_agent import agente
-from src.state import AgentState
-from src.tools import all_tools
-
-workflow = StateGraph(AgentState)
-workflow.add_node("agente", agente)
-workflow.add_node("tools", ToolNode(all_tools))
-
-workflow.set_entry_point("agente")
-workflow.add_conditional_edges("agente", tools_condition, {"tools": "tools", END: END})
-workflow.add_edge("tools", "agente")
-
-grafo = workflow.compile()
--- src/state.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-
-from langgraph.graph.message import add_messages
-from typing_extensions import TypedDict
-
-class AgentState(TypedDict):
-    """Represents the state of an agent in the system."""
-
-    messages: Annotated[list, add_messages]
